a6f9f658232af9077cc394e9e4af700c6b64c8c6
Tobias Kraze <tobias@kraze.eu>
2012-12-14 17:51:14 +0100

    improve patiently do with nested wait_untils

--- lib/spreewald_support/tolerance_for_selenium_sync_issues.rb
+++ lib/spreewald_support/tolerance_for_selenium_sync_issues.rb
@@ -3,6 +3,9 @@
 module ToleranceForSeleniumSyncIssues
   # This is similiar but not entirely the same as Capybara::Node::Base#wait_until or Capybara::Session#wait_until
   def patiently(seconds=Capybara.default_wait_time, &block)
+    old_wait_time = Capybara.default_wait_time
+    # dont make nested wait_untils use up all the alloted time
+    Capybara.default_wait_time = 0 # for we are a jealous gem
     if page.driver.wait?
       start_time = Time.now
       begin
@@ -16,6 +19,8 @@ module ToleranceForSeleniumSyncIssues
     else
       block.call
     end
+  ensure
+    Capybara.default_wait_time = old_wait_time
   end
 end
 
--- lib/spreewald_support/version.rb
+++ lib/spreewald_support/version.rb
@@ -1,5 +1,5 @@
 # coding: UTF-8
 
 module Spreewald
-  VERSION = "0.3.7"
+  VERSION = "0.3.8"
 end
