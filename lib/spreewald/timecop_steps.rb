3cc0e518e50552472d03e94c11926b940fead7ff
Henning Koch <henning.koch@makandra.de>
2013-02-11 12:42:36 +0100

    make quotes around dates and times optional in timecop steps

--- lib/spreewald/timecop_steps.rb
+++ lib/spreewald/timecop_steps.rb
@@ -10,16 +10,16 @@ if defined?(Timecop)
 
   # Example:
   #
-  #       Given the date is "2012-02-10"
-  #       Given the time is "2012-02-10 13:40"
-  When /^the (?:date|time) is "(\d{4}-\d{2}-\d{2}(?: \d{1,2}:\d{2})?)"$/ do |time|
+  #       Given the date is 2012-02-10
+  #       Given the time is 2012-02-10 13:40
+  When /^the (?:date|time) is "?(\d{4}-\d{2}-\d{2}(?: \d{1,2}:\d{2})?)"?$/ do |time|
     Timecop.travel Time.parse(time)
   end
 
   # Example:
   #
-  #       Given the time is "13:40"
-  When /^the time is "(\d{1,2}:\d{2})"$/ do |time|
+  #       Given the time is 13:40
+  When /^the time is "?(\d{1,2}:\d{2})"?$/ do |time|
     Timecop.travel Time.parse(time) # date will be today
   end
 
