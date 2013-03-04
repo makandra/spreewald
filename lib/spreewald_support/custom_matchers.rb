0f7e886b49be161c3e73c09b0f1c8fcd7bb60478
Dominik Schöler <dominik.schoeler@makandra.de>
2013-03-04 17:05:47 +0100

    Enable exact matching and wildcard (*) matching in field-should-contain steps

---
+++ lib/spreewald_support/custom_matchers.rb
@@ -0,0 +1,22 @@
+module CustomMatchers
+  rspec = defined?(RSpec) ? RSpec : Spec
+
+  rspec::Matchers.define :contain_with_wildcards do |expected_string|
+    match do |field_value|
+      @field_value = field_value
+      @expected_string = expected_string
+      regex_parts = expected_string.split('*', -1).collect { |part| Regexp.escape(part) }
+      
+      @field_value =~ /\A#{regex_parts.join(".*")}\z/
+    end
+
+    failure_message_for_should do
+      "The field's content #@field_value did not match #@expected_string"
+    end
+
+    failure_message_for_should_not do
+      "The field's content #@field_value matches #@expected_string"
+    end
+  end
+  
+end
