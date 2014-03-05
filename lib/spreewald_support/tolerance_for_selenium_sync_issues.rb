9fd6200d975ada613f100f5858747154367d1060
Martin Straub <martin.straub@makandra.de>
2014-03-05 17:48:01 +0100

    add Selenium::WebDriver::Error::NoSuchFrameError to ToleranceForSeleniumSyncIssues

--- lib/spreewald_support/tolerance_for_selenium_sync_issues.rb
+++ lib/spreewald_support/tolerance_for_selenium_sync_issues.rb
@@ -10,6 +10,7 @@ module ToleranceForSeleniumSyncIssues
     Selenium::WebDriver::Error::StaleElementReferenceError
     Selenium::WebDriver::Error::NoAlertPresentError
     Selenium::WebDriver::Error::ElementNotVisibleError
+    Selenium::WebDriver::Error::NoSuchFrameError
   ]
 
   # This is similiar but not entirely the same as Capybara::Node::Base#wait_until or Capybara::Session#wait_until
--- lib/spreewald_support/version.rb
+++ lib/spreewald_support/version.rb
@@ -1,3 +1,3 @@
 module Spreewald
-  VERSION = '0.9.3'
+  VERSION = '0.9.4'
 end
