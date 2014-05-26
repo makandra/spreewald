88d9cd9d09fbab209485332eb71fbbf110232005
Dominik Schöler <dominik.schoeler@makandra.de>
2014-05-26 14:37:02 +0200

    Fix "field should not have an error" step: do not generate invalid CSS selectors

--- lib/spreewald/web_steps.rb
+++ lib/spreewald/web_steps.rb
@@ -249,6 +249,7 @@ Then /^the "([^\"]*)" field should( not)? have an error$/ do |label, negate|
   patiently do
     expectation = negate ? :should_not : :should
     field = find_field(label)
+    field[:id].should be_present # prevent bad CSS selector if field lacks id
     page.send(expectation, have_css(".field_with_errors ##{field[:id]}"))
   end
 end
