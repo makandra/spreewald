makandra/spreewald

commit dd339131b63888419c2534bab75188beeb649828

Improve "within"-step: Avoid nesting of the given step in a patiently block
as this would make all patiently-calls in the step useless
(nested patiently calls get a wait time of 0. Hence they are not patiently at all, see https://makandracards.com/makandra/44919)

Also remove byebug dependency from test projects to make them work with ruby <2 again.

lib/spreewald/web_steps.rb
@@ -40,8 +40,9 @@ require 'cgi'
 #     Then I should see "some text" within ".page_body"
 When /^(.*) within (.*[^:])$/ do |nested_step, parent|
   patiently do
-    with_scope(parent) { step(nested_step) }
+    page.should have_css(_selector_for(parent))
   end
+  with_scope(parent) { step(nested_step) }
 end.overridable(:priority => 10)
 
 # nodoc

lib/spreewald_support/version.rb
@@ -1,3 +1,3 @@
 module Spreewald
-  VERSION = '1.8.1'
+  VERSION = '1.9.0'
 end

tests/rails-2.3/Gemfile
@@ -3,7 +3,6 @@ source 'https://rubygems.org'
 gem 'rails', '~>2.3'
 
 gem "sqlite3", :platform=>:ruby
-gem 'ruby-debug', :platforms => :ruby_18
 gem 'database_cleaner'
 gem 'selenium-webdriver', '>=2.35.0'
 gem "capybara", "1.1.1"

tests/rails-3.2/capybara-1/Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 gem 'rails', '~>3.2'
 gem "sqlite3", :platform=>:ruby
-gem 'ruby-debug', :platforms => :ruby_18
 gem 'database_cleaner'
 gem 'capybara', "~>1" # for ruby 1.8.7 compatibility
 gem 'cucumber-rails'

tests/rails-3.2/capybara-2/Gemfile
@@ -14,5 +14,3 @@ gem 'rspec-rails'
 gem 'spreewald', :path => '../../..'
 
 gem 'haml-rails'
-gem 'byebug'
-

commit f586d0584f114d0be82cc269f86d75f65c891cf6

Fix "within"-step to again work with Capybara::Node::Elements
Got broken with dd339131 / 1.9.0

lib/spreewald/web_steps.rb
@@ -39,8 +39,11 @@ require 'cgi'
 #
 #     Then I should see "some text" within ".page_body"
 When /^(.*) within (.*[^:])$/ do |nested_step, parent|
-  patiently do
-    page.should have_css(_selector_for(parent))
+  selector = _selector_for(parent)
+  if selector.is_a?(String) # could also be a Capybara::Node::Element
+    patiently do
+      page.should have_css(selector)
+    end
   end
   with_scope(parent) { step(nested_step) }
 end.overridable(:priority => 10)

lib/spreewald_support/version.rb
@@ -1,3 +1,3 @@
 module Spreewald
-  VERSION = '1.9.2'
+  VERSION = '1.9.3'
 end

tests/shared/app/views/static_pages/within.html.haml
@@ -2,3 +2,15 @@
   .child1
 
 .container2
+
+%table
+  %tr
+    %th Role
+    %th Permissions
+  %tr
+    %td User
+    %td None
+  %tr
+    %td Admin
+    %td All
+

tests/shared/features/shared/web_steps.feature
@@ -130,6 +130,10 @@ Feature: Web steps
     Then I should see an element ".child1" within ".container1"
       But I should not see an element ".child1" within ".container2"
 
+  Scenario: /^(.*) within (.*[^:])$/ with a Capybara::Node::Element
+    When I go to "/static_pages/within"
+    Then I should see "All" within the table row containing "Admin"
+
   Scenario: /^I perform basic authentication as "([^\"]*)\/([^\"]*)" and go to (.*)$/
     When I go to "/authenticated/page"
     Then I should see "Access denied"

tests/shared/features/support/selectors.rb
@@ -14,6 +14,9 @@ module HtmlSelectorsHelpers
     when /^the timeline?$/
       '.timeline'
 
+    when /^the table row containing "(.+?)"$/
+      all('tr').detect { |tr| tr.text.include? $1 } || raise("Could not find tr containing #{$1.inspect}")
+
       # Add more mappings here.
       # Here is an example that pulls values out of the Regexp:
       #