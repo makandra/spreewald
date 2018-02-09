3aa9220dd9ff967238fec221945ec5f737bfbece
Henning Koch <henning.koch@makandra.de>
2018-02-09 10:44:03 +0100

    Make "I should NOT see a field" compatible with patiently failures

--- lib/spreewald/web_steps.rb
+++ lib/spreewald/web_steps.rb
@@ -314,13 +314,18 @@ Then /^I should( not)? see a field "([^"]*)"$/ do |negate, name|
   expectation = negate ? :should_not : :should
   patiently do
     begin
+      puts "Spreewald calling find field: #{method(:find_field).source_location.inspect}"
+      # In old Capybaras find_field returns nil, so we assign it to `field`
       field = find_field(name)
     rescue Capybara::ElementNotFound
       # In Capybara 0.4+ #find_field raises an error instead of returning nil
+      # We must explicitely reset the field variable from a previous patiently iteration
+      field = nil
     end
     field.send(expectation, be_present)
   end
 end.overridable
+
 
 # Use this step to test for a number or money amount instead of a simple `Then I should see`
 #
