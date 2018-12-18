90aab1f342ef5559e53ae6d669afbf1bf82cd1fb
Henning Koch <henning.koch@makandra.de>
2018-12-18 19:09:07 +0100

    Use Gemika to run tests and drop rails 2.3 test projects

--- tests/rails-2.3/config/environments/test.rb
+++
@@ -1,3 +0,0 @@
-RSPEC_EXPECTATION_NOT_MET_ERROR = Spec::Expectations::ExpectationNotMetError
-
-config.action_mailer.delivery_method = :test
--- tests/rails-2.3/config/routes.rb
+++
@@ -1,5 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect ':controller/:action'
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
-end
